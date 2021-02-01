c661301f5a75e6c94f37290e3349b35ea1af5f48
Frankie Panayi <74913808+FrankiePanayi@users.noreply.github.com>
2021-02-01 18:46:51 +0000

    Add files via upload
    Python Strings.

---
+++ Python Strings.py
@@ -0,0 +1,9 @@
+first_name = 'frankie'
+surname = 'Panayi'
+
+full_name = first_name.capitalize() + ' ' + surname
+
+print (full_name)
+print (len(first_name))
+print (len(surname))
+print (full_name.endswith('Panayi'))
