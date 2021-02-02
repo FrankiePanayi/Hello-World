d2d1bc39692834c68a095f07bcb4005a492ef22a
Frankie Panayi <74913808+FrankiePanayi@users.noreply.github.com>
2021-02-02 15:25:08 +0000

    Add files via upload
    Brief Intro to a story using python

---
+++ Python Story Intro.py
@@ -0,0 +1,7 @@
+character_name = "Frankie"
+character_name2 = "Danny"
+character_name2mum = "Jennifer"
+dinner = "fish and chips"
+
+print("One day " + character_name + " was playing football with " + character_name2)
+print (" After several hours of playing fotball together " + character_name2mum + "," + " " + character_name2  + "'s" + " " + "mum" +" " + "called him inside for " + dinner)
