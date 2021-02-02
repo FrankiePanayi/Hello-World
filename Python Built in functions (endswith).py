f8ed021bd396d56d1b66ce519f35534cd79f1ae3
Frankie Panayi <74913808+FrankiePanayi@users.noreply.github.com>
2021-02-02 17:10:41 +0000

    Add files via upload

---
+++ Python Built in functions (endswith).py
@@ -0,0 +1,12 @@
+def check_age(age):
+    if age <18:
+        print ('ooops not an adult')
+    else:
+        print('hooray im an adult')
+
+print('hello'.endswith('O'))
+print('hello'.endswith('o'))
+
+check_age(18)
+check_age(17)
+check_age(99)
